f3a6d939d3209ded90c475d81eb6a4d00c146326
danstanek <97788985+danstanek@users.noreply.github.com>
2022-01-16 10:57:39 +0100

    Update aiport.php

--- EE.09-06-21.01ODLOTY_Z+LOTNISKA/aiport.php
+++ EE.09-06-21.01ODLOTY_Z+LOTNISKA/aiport.php
@@ -46,17 +46,12 @@
 	<div id="cookie">
 		<p>
         <?php
-			if (isset($_COOKIE["ciasteczko"])) {
+			if (isset($_COOKIE["ciasteczko"])) 
+			
+			{
 				echo "<b>Miło nam, że nas znowu odwiedziłeś</b>";
 			} else {
-				$cookie_name = "cookie";
-				$cookie_value = "";
-				$cookie_time = time() + 60*60*2;
-				$cookie_path = "";
-				$cookie_domain = "";
-				$cookie_secure  = false;
-				$cookie_httponly = false;
-				setcookie($cookie_name, $cookie_value, $cookie_time, $cookie_path, $cookie_domain, $cookie_secure, $cookie_httponly);
+				setcookie("ciasteczko", 1, time()+3600);
 				echo "<p style='font-style: italic;'>Sprawdź regulamin naszej strony!</p>";
 			}
  ?>
