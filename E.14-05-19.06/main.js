78419cf4e44eb5d5fbaf23f58f6b6c32f59a6ebc
danstanek <97788985+danstanek@users.noreply.github.com>
2024-01-11 19:11:05 +0100

    Add files via upload

---
+++ E.14-05-19.06/main.js
@@ -0,0 +1,12 @@
+function formularz() {
+	let x = document.getElementById("check").checked;
+	if(x) {
+		let wynik = document.getElementById("wynik");
+		let imie = (document.getElementById("imie").value).toUpperCase();
+		let nazwisko = (document.getElementById("nazwisko").value).toUpperCase();
+		let usluga = document.getElementById("usluga").value;
+		wynik.innerHTML = imie + " " + nazwisko + "<br/>" + "Treść Twojej sprawy: " + usluga + "<br/>" + "Na podany e-mail zostanie wysłana oferta."; 
+	} else {
+		wynik.innerHTML = "<span style='color: red;'>Musisz zapoznać się z regulaminem</span>";
+	}
+}
